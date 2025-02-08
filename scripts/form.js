e562f4907c4afd23bde84abeda7a893dd28eeda7
Norlando_jr <norlandojr@hotmail.com>
2025-02-08 09:49:39 -0300

    commit

--- scripts/form.js
+++ scripts/form.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let reviewCount = localStorage.getItem("reviewCount") || 0;
-
-    document.querySelector("form").addEventListener("submit", function () {
-        reviewCount++;
-        localStorage.setItem("reviewCount", reviewCount);
-
-        const userName = document.getElementById("user-name").value;
-        localStorage.setItem("userName", userName);
+    document.addEventListener("DOMContentLoaded", function () {
+        let reviewCount = localStorage.getItem("reviewCount") || 0;
+    
+        document.querySelector("form").addEventListener("submit", function () {
+            reviewCount++;
+            localStorage.setItem("reviewCount", reviewCount);
+    
+            const userName = document.getElementById("user-name").value;
+            localStorage.setItem("userName", userName);
+        });
+    
+        if (document.getElementById("reviewCounter")) {
+            document.getElementById("reviewCounter").textContent = `Reviews Submitted: ${reviewCount}`;
+        }
     });
-
-    if (document.getElementById("reviewCounter")) {
-        document.getElementById("reviewCounter").textContent = `Reviews Submitted: ${reviewCount}`;
-    }
-});
+    
